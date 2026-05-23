854cd49af27fd556ba8861e7e7a685cbd703c25d
Lenny Reinkensmeier <lrein@live.de>
2026-05-23 17:18:32 +0200

    Add KinesisRotatorManager for Thorlabs K10CR1 rotators

---
+++ imswitch/imcontrol/model/managers/rotators/__init__.py
@@ -0,0 +1,9 @@
+from .KinesisRotatorManager import KinesisRotatorManager
+from .RotatorManager import RotatorManager
+from .StandaRotatorManager import StandaRotatorManager
+
+__all__ = [
+    'KinesisRotatorManager',
+    'RotatorManager',
+    'StandaRotatorManager',
+]
