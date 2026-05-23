e9527e28588d9d9da9a78a7ef8dd1d88e1e028dc
Lenny Reinkensmeier <lrein@live.de>
2026-05-23 17:21:19 +0200

    Add ElliptecRotatorManager with shared multidrop bus

--- imswitch/imcontrol/model/managers/rotators/__init__.py
+++ imswitch/imcontrol/model/managers/rotators/__init__.py
@@ -1,8 +1,10 @@
+from .ElliptecRotatorManager import ElliptecRotatorManager
 from .KinesisRotatorManager import KinesisRotatorManager
 from .RotatorManager import RotatorManager
 from .StandaRotatorManager import StandaRotatorManager
 
 __all__ = [
+    'ElliptecRotatorManager',
     'KinesisRotatorManager',
     'RotatorManager',
     'StandaRotatorManager',
